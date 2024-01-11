b87a94fc47e7f0dff659ba1b7521aaf69b973b54
ClearAspect <roanmason@live.ca>
2024-01-11 13:41:21 -0500

    Started the first lab

---
+++ Maso2187_l01/src/t01.py
@@ -0,0 +1,16 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  Roan Somers Mason
+ID:      169072187
+Email:   maso2187@mylaurier.ca
+__updated__ = "2024-01-10"
+-------------------------------------------------------
+"""
+# Imports
+from Food import Food
+
+# Constants
+
+print(Food.origins())
